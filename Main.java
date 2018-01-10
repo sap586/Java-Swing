01525ecff11ddd33f99d231cc32a62b126827bad
sap586 <sap586@nyu.edu>
2018-01-10 06:16:04 -0500

    JRadioButton

--- Main.java
+++ Main.java
@@ -5,25 +5,38 @@ import java.awt.event.ActionEvent;
 
 class Test {
     public static void main(String[] args) {
+        
+        
+        
         A a = new A();
+        
+        
+        
     }
 }
 
 class A extends JFrame implements ActionListener {
-    JTextField t1;
-    JTextField t2;
+    JTextField t1, t2;
     JButton b;
+    JRadioButton r1, r2;
     JLabel l;
+    ButtonGroup bg = new ButtonGroup();
     A() {
         t1 = new JTextField(30);
         t2 = new JTextField(30);
         b = new JButton("Add");
         l = new JLabel("Result");
+        r1 = new JRadioButton("Add");
+        r2 = new JRadioButton("Sub");
+        bg.add(r1);
+        bg.add(r2);
         b.addActionListener(this);
         add(t1);
         add(t2);
         add(b);
         add(l);
+        add(r1);
+        add(r2);
         
         setLayout(new FlowLayout());
         setVisible(true);
@@ -33,7 +46,15 @@ class A extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent ae) {
         int a = Integer.parseInt(t1.getText());
         int b = Integer.parseInt(t2.getText());
-        int c = a + b;
+        int c;
+        if (r1.isSelected()) {
+            c = a + b;
+        } else if (r2.isSelected()) {
+            c = a - b;
+        } else {
+            l.setText("Invalid");
+            return;
+        }
         l.setText(c + "");
     }
 }
